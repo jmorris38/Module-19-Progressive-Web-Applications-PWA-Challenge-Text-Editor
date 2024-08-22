d79fae569f28949b05bd285e60bfb2cc890a1bad
Justin Morris <jmorris38@outlook.com>
2024-08-22 07:21:45 +0500

    make the editor installable, webpack configured

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,15 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+// These are the dependencies for workbox.
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+// This is used to precache the workbox manifest.
 precacheAndRoute(self.__WB_MANIFEST);
-
+// This is used to create a new instance of the cache first strategy.
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -18,13 +19,31 @@ const pageCache = new CacheFirst({
     }),
   ],
 });
-
-warmStrategyCache({
-  urls: ['/index.html', '/'],
+// This offline fallback is used to cache the index.html file.
+offlineFallback({
+  pageFallback: "/index.html",
+  pageFallbackDependencies: ["/index.html"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// This warm strategy cache is used to cache the index.html file.
+warmStrategyCache({
+  urls: ["/index.html", "/"],
+  strategy: pageCache,
+});
+// This register route is used to cache the page.
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// This register route is used to cache the assets.
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,31 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Logic for installing the PWA.
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Logic for handling the install button.
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    // Wait for the user to respond to the prompt.
+    promptEvent.prompt();
+    // Clear the deferredPrompt variable.
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Logic for handling the app installation event.
+window.addEventListener('appinstalled', (event) => {
+    console.log('App was installed');
+    butInstall.style.display = 'none';
+    // Clear the prompt.
+    window.deferredPrompt = null;
+});
