ff3f34ebea4ef05c3eb97dc6ecd6747b4c829392
Justin Morris <jmorris38@outlook.com>
2024-08-22 06:56:27 +0500

    server created and api routes generated

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,6 @@
+// This variable for requiring a dependency is used to join the path to the index.html file in the client/dist folder.
 const path = require('path');
-
+// The app parameter is used to get the index.html file from the client/dist folder and send it to the browser.
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
+// Require the dependencies.
 const express = require('express');
-
+// Use app to call express methods and set the PORT to 3000.
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+// Use the express static method to serve the browser the files in the client/dist folder.
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Require the html Routes file and pass in the app so that the routes can be used.
 require('./routes/htmlRoutes')(app);
-
+// This uses the listen method to start the server on the PORT.
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
